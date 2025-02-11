02f89429a89437d2fbabc2cf80f5f40e96d79dd3
Shanon Giles <sgiles21@ivytech.edu>
2025-02-10 22:45:23 -0600

    Make changes to App.Jsx and Home.jsx, deleted 3 jsx files

--- routing/src/App.jsx
+++ routing/src/App.jsx
@@ -1,22 +1,18 @@
 import './App.css'
 import {Route, Routes} from 'react-router-dom'
 import { Home } from './components/Home'
-import { Number } from './components/Number'
-import { Greeting } from './components/Greeting'
-import { HelloColor } from './components/HelloColor'
 
 export const App = () => {
   
   return (
     <>
       <Routes>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/4/:number" element={<Number/>}/>
-        <Route path="/hello/:greeting" element={<Greeting/>}/>
-        <Route path="/hello/:textColor/:backGroundColor" element={<HelloColor/>}/>
+        <Route path="/home" element={<Home />} />
+        <Route path="/:value" element={<Home />} />
+        <Route path="/:value/:color/:backgroundcolor" element={<Home />} />
       </Routes>
     </>
-  )
+  );
 }
 
 
--- routing/src/components/Home.jsx
+++ routing/src/components/Home.jsx
@@ -1,10 +1,31 @@
+import { useParams } from "react-router-dom";
 
 export const Home = () => {
+  const { value, color, backgroundcolor } = useParams();
 
-    return(
+  
+  if (!value) {
+    return <h1>Welcome</h1>;
+  }
 
-        <>
-        <h1>Welcome!</h1>
-        </>
-    )
-}
+ 
+  if (!isNaN(Number(value))) {
+    return <h1>The number is: {value}</h1>;
+  }
+
+  
+  if (value && !color && !backgroundcolor) {
+    return <h1>The word is: {value}</h1>;
+  }
+
+  
+  if (value && color && backgroundcolor) {
+    return (
+      <h1 style={{ color: color, backgroundColor: backgroundcolor }}>
+        The word is: {value}
+      </h1>
+    );
+  }
+
+  return <h1>Invalid Input</h1>;
+};
